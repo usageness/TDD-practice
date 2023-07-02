5ca3585528b2141a4587925b45a1b89bb3deafce
YongRae_Kim (Usage) <kyr9389@naver.com>
2023-07-02 17:12:16 +0900

    feat: times() 기능 구현 및 테스트 작성

--- __test__/example.test.js
+++ __test__/example.test.js
@@ -1,7 +1,14 @@
+import Dollar from "../src/money";
+
 describe("TDD 연습을 위한 테스트입니다.", () => {
   beforeAll(() => {});
 
-  it("예시 테스트 1번", () => {
-    expect(1).toBe(1);
+  it("오류 수정 예제", () => {
+    const five = new Dollar(5);
+    let product = five.times(2);
+    expect(product.amount).toBe(10);
+
+    product = five.times(3);
+    expect(product.amount).toBe(15);
   });
 });
