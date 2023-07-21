c4441a07a46a16fee90dfe61d90595d9b77fb3a1
YongRae_Kim (Usage) <kyr9389@naver.com>
2023-07-21 16:13:03 +0900

    refactor: equal() 메서드 리팩토링

--- __test__/example.test.js
+++ __test__/example.test.js
@@ -5,11 +5,8 @@ describe("TDD 연습을 위한 테스트입니다.", () => {
 
   it("오류 수정 예제", () => {
     const five = new Dollar(5);
-    let product = five.times(2);
-    expect(product.amount).toBe(10);
-
-    product = five.times(3);
-    expect(product.amount).toBe(15);
+    expect(new Dollar(10)).toStrictEqual(five.times(2));
+    expect(new Dollar(15)).toStrictEqual(five.times(3));
   });
 
   it("equals() 테스트", () => {
