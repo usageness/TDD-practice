808c6d9c7d6f7442e5f2c2ce89db72b16f893bb6
YongRae_Kim (Usage) <kyr9389@naver.com>
2023-08-20 17:20:08 +0900

    refactor: 팩토리 메서드를 통해 dollar, franc 구현하도록 수정

--- __test__/example.test.js
+++ __test__/example.test.js
@@ -1,25 +1,25 @@
-import { Dollar, Franc } from "../src/money";
+import { Money } from "../src/money";
 
 describe("TDD 연습을 위한 테스트입니다.", () => {
   beforeAll(() => {});
 
   it("Dollar 테스트", () => {
-    const five = new Dollar(5);
-    expect(new Dollar(10)).toStrictEqual(five.times(2));
-    expect(new Dollar(15)).toStrictEqual(five.times(3));
+    const five = Money.dollar(5);
+    expect(Money.dollar(10)).toStrictEqual(five.times(2));
+    expect(Money.dollar(15)).toStrictEqual(five.times(3));
   });
 
   it("Franc 테스트", () => {
-    const five = new Franc(5);
-    expect(new Franc(10)).toStrictEqual(five.times(2));
-    expect(new Franc(15)).toStrictEqual(five.times(3));
+    const five = Money.franc(5);
+    expect(Money.franc(10)).toStrictEqual(five.times(2));
+    expect(Money.franc(15)).toStrictEqual(five.times(3));
   });
 
   it("equals() 테스트", () => {
-    expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
-    expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
-    expect(new Franc(5).equals(new Franc(5))).toBe(true);
-    expect(new Franc(5).equals(new Franc(6))).toBe(false);
-    expect(new Franc(5).equals(new Dollar(5))).toBe(false);
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
+    expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
+    expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
+    expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
   });
 });
--- src/money.js
+++ src/money.js
@@ -5,6 +5,14 @@ class Money {
     this.amount = amount;
   }
 
+  static dollar(amount) {
+    return new Dollar(amount);
+  }
+
+  static franc(amount) {
+    return new Franc(amount);
+  }
+
   equals(object) {
     return object instanceof this.constructor && this.amount === object.amount;
   }
@@ -36,4 +44,4 @@ class Franc extends Money {
   }
 }
 
-export { Dollar, Franc };
+export { Money, Dollar, Franc };
